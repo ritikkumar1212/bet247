afaa1bfe8383c19cf0d640c3c95b21c2ff214f4a
Ritik <ritik.kumar16@yahoo.com>
2026-02-22 16:42:42 +0530

    app.ts update

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,11 +1,13 @@
 import express from "express";
 import cors from "cors";
-import routes from "./routes";
+import routes from "./routes";import ingestRoutes from "./modules/ingest/ingest.routes";
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
+app.use("/api/ingest", ingestRoutes);
+
 
 app.use(routes);
 
--- backend/src/modules/ingest/ingest.routes.ts
+++ backend/src/modules/ingest/ingest.routes.ts
@@ -3,6 +3,6 @@ import { ingestBallController } from "./ingest.controller";
 
 const router = Router();
 
-router.post("/ingest/ball", ingestBallController);
+router.post("/ball", ingestBallController);
 
 export default router;
